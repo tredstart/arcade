d6181d017ac597b400c2a6f71a0f767be5ef466f
redstart <rslnkssk@gmail.com>
2023-12-07 18:08:07 +0100

    add a bunch of boilerplate for a user

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -13,6 +13,21 @@ func (s *Server) RegisterRoutes() http.Handler {
 	e.Use(middleware.Recover())
 
 	e.GET("/", s.helloWorldHandler)
+    // and this should be account thin idk
+    // also how do I do "save to the local storage and shit?"
+    // e.GET("/login")
+    // e.GET("/signup")
+    // e.GET("/home") -> should probably show the latest retro
+    // e.GET("/history")
+
+    // basically the whole retro logic that i can group into retro package
+    // e.GET("/retro/:url") -> should verify if user is in local storage or some shit
+    // e.POST("/retro/:url") -> does a post to create an item in retro box
+    // e.GET("/retro/new")
+    // e.POST("/retro/new")
+    // e.GET("/retro/templates")
+    // e.GET("/retro/templates/new")
+    // e.POST("/retro/templates/new")
 
 	return e
 }
